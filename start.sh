206ea7f8ff56e51f1b1b7c4178b3e21a2dca61fd
ehipolito <estiverherrerahipolito@gmail.com>
2018-03-11 13:13:29 -0300

    retirado variavel de ambiente para apontar para o consul

--- start.sh
+++ start.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 java -Xmx64m -jar poc.jar &
-/opt/springboot/poc/consul-template -consul-addr http://$CONSUL_HOST -template "/opt/springboot/poc/config/application.template:/opt/springboot/poc/config/application.properties:./refresh.sh"
+/opt/springboot/poc/consul-template -consul-addr http://k8s-poc-consul-service -template "/opt/springboot/poc/config/application.template:/opt/springboot/poc/config/application.properties:./refresh.sh"
 
