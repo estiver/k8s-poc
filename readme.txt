84f1beda422484584df2a67e60152985d2376cea
ehipolito <estiverherrerahipolito@gmail.com>
2018-04-21 16:44:27 -0300

    servico poc conectado com person

--- readme.txt
+++ readme.txt
@@ -1,3 +1,6 @@
 sudo docker run -d --net=host consul:latest
 sudo docker build -t ehh-poc .
+
+sudo docker build -t ehipolito/k8s-poc .
+sudo docker push ehipolito/k8s-poc
 sudo docker run --name=poc --net="host" -it  ehh-poc
--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 java -Xmx64m -jar poc.jar &
 /opt/springboot/poc/consul-template \
     -consul-addr http://k8s-poc-consul-service \
-    -vault-renew-token=false \ 
+    -vault-renew-token=false \
     -vault-addr http://k8s-poc-vault-service:8200 \
     -template "/opt/springboot/poc/config/application.template:/opt/springboot/poc/config/application.properties:./refresh.sh"
 
